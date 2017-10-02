69f0c2d8050da0bec0f209a8ea9e9990662ab92e
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-10-02 14:22:31 +0800

    Updated view controller

--- codeigniter/application/controllers/Main.php
+++ codeigniter/application/controllers/Main.php
@@ -8,7 +8,7 @@ class Main extends MY_View_Controller {
   }
   
   public function index() {
-    $this->_view();
+    $this->_view('pages/home', 'CI Project Setup');
   }
 }
 
--- codeigniter/application/core/MY_Controller.php
+++ codeigniter/application/core/MY_Controller.php
@@ -23,54 +23,55 @@ class MY_View_Controller extends MY_Controller {
   }
 
   /**
-   * Checks if $view exists and Loads it.
-   *
-   * @param string $view The name of the view to be loaded
-   * @param array $data An associative array to be passed in the $view
-   * @return void
-   */
-  private function _load_view($view, $data = array()) {
-    // check if page exists
-    if (!file_exists(APPPATH . 'views/' . $view . '.php')) {
-      show_404();
-    }
-    // load view
-    $this->load->view($view, $data);
-  }
-
-  /**
    * Loads a view in the views/ directory.
    *
    * @param mixed $view Name of the view or an array of names of the views in views/ to be loaded
-   * @param array $data An associative array to be passed in the $view
-   * @param boolean $page_only Loads the $view only; otherwise, header and footer in views/templates/ are also loaded
+   * @param mixed $data A string title for the view or an associative array to be passed in the $view
+   * @param boolean $view_only Loads the $view only; otherwise, header and footer in views/templates/ are also loaded
    * @return void
    */
-  protected function _view($view = 'pages/home', $data = array(), $page_only = FALSE) {
+  protected function _view($view = 'pages/home', $data = array(), $view_only = FALSE) {
+    $header = 'templates/header';
+    $footer = 'templates/footer';
 
-    // load header
-    if (!$page_only) {
-      // set title if data is null or empty
-      if (empty($data) && !is_array($view)) {
-        $data['title'] = ucfirst(basename($view));
-      }
+    // create array of views
+    $views = array();
 
-      // load header
-      $this->_load_view('templates/header', $data);
+    // add header
+    if (!$view_only) {
+      array_push($views, $header);
     }
 
     // turn $view into array
-    if (!is_array($view)) {
+    if (!is_array($title = $view)) {
       $view = array($view);
     }
-    // load views
-    foreach ($view as $v) {
-      $this->_load_view($v, $data);
+    // add $view
+    $views = array_merge($views, $view);
+
+    // add footer
+    if (!$view_only) {
+      array_push($views, $footer);
     }
 
-    // load footer
-    if (!$page_only) {
-      $this->_load_view('templates/footer');
+    // turn $data into array with title
+    if (is_string($data)) {
+      $data = array('title' => $data);
+    }
+
+    // set title if data is null or empty
+    if (empty($data) && is_string($title)) {
+      $data['title'] = ucfirst(basename($title));
+    }
+
+    // load $views
+    foreach ($views as $index => $view) {
+      // check if page exists
+      if (!file_exists(APPPATH . 'views/' . $view . '.php')) {
+        show_404();
+      }
+      // load view
+      $this->load->view($view, $index === 0 ? $data : NULL);
     }
 
   }
